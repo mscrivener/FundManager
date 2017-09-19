4e6889f4dd12b6dac4b120da3a259eea91c20fcd
M Scrivener <maria.skovmand@hotmail.com>
2017-09-19 12:48:32 +0100

    Initial setup of project

---
+++ FundManager.Test/PortfolioValueTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FundManager.Test
+{
+    [TestClass]
+    public class PortfolioValueTester
+    {
+        [TestMethod]
+        public void Test_HoldingValueSum()
+        {
+        }
+    }
+}
---
+++ FundManager.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FundManager.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FundManager.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("190ae73e-f26d-40af-8814-6f2d4781ae92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
